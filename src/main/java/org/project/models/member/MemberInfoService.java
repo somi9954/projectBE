6c9506bc023aeeb7792002b3c9e1b5632dcbe743
somi9954 <jolh999@gmail.com>
2023-12-24 19:07:01 +0900

    회원가입 기능 구현중

---
+++ src/main/java/org/project/models/member/MemberInfoService.java
@@ -0,0 +1,39 @@
+package org.project.models.member;
+
+import lombok.RequiredArgsConstructor;
+import org.project.commons.contansts.MemberType;
+import org.project.entities.Member;
+import org.project.repositories.MemberRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+@Service
+@RequiredArgsConstructor
+public class MemberInfoService implements UserDetailsService {
+
+    private final MemberRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        Member member = repository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException(username));
+
+        MemberType type = Objects.requireNonNullElse(member.getType(), MemberType.USER);
+        List<GrantedAuthority> authorities = Arrays.asList(new SimpleGrantedAuthority(type.name()));
+
+        return MemberInfo.builder()
+                .email(member.getEmail())
+                .password(member.getPassword())
+                .authorities(authorities)
+                .member(member)
+                .build();
+    }
+}
