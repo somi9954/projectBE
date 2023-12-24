ac8f7eb6a4e4e9ca40db0b1ec753483c6593e027
somi9954 <jolh999@gmail.com>
2023-12-24 18:55:58 +0900

    회원가입 기능 구현중

---
+++ src/main/java/org/project/repositories/MemberRepository.java
@@ -0,0 +1,8 @@
+package org.project.repositories;
+
+import org.project.entities.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+
+public interface MemberRepository extends JpaRepository<Member, Long>, QuerydslPredicateExecutor<Member> {
+}
